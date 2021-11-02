ef2047dcaef0aeee24d5a58174ccf562d2a91887
zijia <ilraygo@gmail.com>
2021-11-02 10:39:58 +0800

    update

--- chp6/login_form_requests.py
+++ chp6/login_form_requests.py
@@ -1,4 +1,7 @@
 import requests
+import os
+os.environ["http_proxy"] = "http://192.168.5.1:7890"
+os.environ["https_proxy"] = "http://192.168.5.1:7890"
 
 LOGIN_URL = 'http://example.python-scraping.com/user/login'
 LOGIN_EMAIL = 'example@python-scraping.com'
